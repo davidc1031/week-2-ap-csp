f5b0a865da5e295a448144403d96508319fbb633
davidc1031 <cchuy709@gmail.com>
2025-10-31 17:40:38 +0000

    bepp ringer

--- week_11_day2.py
+++ week_11_day2.py
@@ -16,25 +16,35 @@ favorite_number = 7
 
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
-
+Print(first_name.upper())
 
 # 2. Print how many letters are in your name
+print(len(first_name))
 
 
 # 3. Combine your name and favorite color into one message
-
+print(f"My name is (first_name) and my favorite color is (fav_color)")
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
-
+fav_color = "yellow"
+number_2 = 3
+fav3 = int(fav_color) + int(number_2)
+print(f"my favorite number is plus 3 is(fav3)")
 
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
+number1 = int(input("What is your favorite number?:"))
+number2 = int(input("What is your 2nd favorite number?:"))
+number3 = number1 + number2
+print(f"your favorite number plus your favorite number is (number3)")
 
 
 
 
 # ⚙️ Step 5: Final Challenge (combine it all)
 # Use math and strings together
-
+Fav_word = input("what is your fav word?")
+Fav_word2 = input("what is your 2nd fav word?")
+numberofletters = int(len{fav_word}) + int(len{fav_word2})
 
